339e6de9fac57844ee073a32f9ba9093410a7e88
Jessica Hvirfvel <jessica.hvirfvel@hotmail.com>
2024-11-20 15:16:56 +0100

    hero video

--- src/components/About.js
+++ src/components/About.js
@@ -53,7 +53,7 @@ const About = () => {
             </Helmet>
             <div className=" ">
                 <div className='about-hero'>
-                    <video className='hero-video' autoPlay muted loop>
+                    <video className='hero-video' autoPlay muted loop playsInline>
                         <source src={heroVideo} type='video/mp4' />
                         Your browser does not support the video tag.
                     </video>
