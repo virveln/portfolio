0f12b112ed0bf224a86b3b2dac281deeae1896ef
Jessica Hvirfvel <jessica.hvirfvel@hotmail.com>
2025-01-21 13:06:31 +0100

    update certificate code and cv

--- src/components/about/Certificates.js
+++ src/components/about/Certificates.js
@@ -4,29 +4,22 @@ import '../../styles/About.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { IoOpenOutline } from "react-icons/io5";
-import UnderstandingTypeScript from '../../images/certificates/Udemy_Certificate_Understanding_Typescript_Hvirfvel.jpg';
-import UnitTesting from '../../images/certificates/Udemy_Certificate_Unit_Testing_TS_Node_Jest_Hvirfvel.jpg';
+import certificateData from '../../data/certificatesData';
 
 const Certificate = ({ aboutData, t }) => {
     return (
         <div className='certificate-container new-container'>
             <h2>Certificates<span className='colorfulend'>.</span></h2>
-            <div className='certificate-item'>
-                <img src={UnderstandingTypeScript} alt='Certificate' />
+            {certificateData.map((certificate) => (
+                <div className='certificate-item'>
+                    <img src={certificate.image} alt='Certificate' />
                 <div className='certificate-info'>
-                    <h4>Understanding TypeScript</h4>
-                    <p>Udemy</p>
-                    <Link to="https://www.udemy.com/certificate/UC-47f25b80-de5e-4d07-915d-285bb0976f9c/" target="_blank" rel="noopener noreferrer" title='Show certificate'>{aboutData.certificateLinkTitle} <IoOpenOutline /></Link>
+                    <h4>{certificate.title}</h4>
+                    <p>{certificate.provider}</p>
+                    <Link to={certificate.link} target="_blank" rel="noopener noreferrer" title='Show certificate'>{aboutData.certificateLinkTitle} <IoOpenOutline /></Link>
                 </div>
-            </div>
-            <div className='certificate-item'>
-                <img src={UnitTesting} alt='Certificate' />
-                <div className='certificate-info'>
-                    <h4>Unit Testing for Typescript & NodeJS Developers with Jest</h4>
-                    <p>Udemy</p>
-                    <Link to="https://www.ude.my/UC-669a9159-05b3-4fa1-96d4-20518b6a4c79" target="_blank" rel="noopener noreferrer" title='Show certificate'>{aboutData.certificateLinkTitle} <IoOpenOutline /></Link>
                 </div>
-            </div>
+            ))}         
         </div>
     );
 };
---
+++ src/data/certificatesData.js
@@ -0,0 +1,22 @@
+
+const typescriptImage = require('../images/certificates/Udemy_Certificate_Understanding_Typescript_Hvirfvel.jpg');
+const unitTestImage = require('../images/certificates/Udemy_Certificate_Unit_Testing_TS_Node_Jest_Hvirfvel.jpg');
+
+const certificateData =[
+    {
+        date: "2025-01-10",
+        title: "Understanding TypeScript",
+        provider: "Udemy",
+        link: "https://www.udemy.com/certificate/UC-47f25b80-de5e-4d07-915d-285bb0976f9c/",
+        image: typescriptImage
+    },
+    {
+        date: "2025-01-17",
+        title: "Unit Testing for Typescript & NodeJS Developers with Jest",
+        provider: "Udemy",
+        link: "https://www.ude.my/UC-669a9159-05b3-4fa1-96d4-20518b6a4c79",
+        image: unitTestImage
+    }
+]
+
+export default certificateData;
